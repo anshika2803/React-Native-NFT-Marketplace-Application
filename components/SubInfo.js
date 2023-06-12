c30800f45ac1d96d985de58d9d5e487a2c2fcd0b
anshika <anshikamaheshwari28@gmail.com>
2023-06-12 13:45:24 +0530

    🚧

---
+++ components/SubInfo.js
@@ -0,0 +1,48 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const NFTTitle = () => {
+  return (
+    <View>
+      <Text>SubInfo</Text>
+    </View>
+  )
+}
+export const EthPrice = () => {
+    return (
+      <View>
+        <Text>ep</Text>
+      </View>
+    )
+  }
+
+export const ImageCmp = () => {
+    return (
+      <View>
+        <Text>Ic</Text>
+      </View>
+    )
+  }
+
+export const People = () => {
+    return (
+      <View>
+        <Text>SubInfo</Text>
+      </View>
+    )
+  }
+export const EndDate = () => {
+    return (
+      <View>
+        <Text>SubInfo</Text>
+      </View>
+    )
+  }
+
+export const SubInfo = () => {
+    return (
+      <View>
+        <Text>SubInfo</Text>
+      </View>
+    )
+  }
