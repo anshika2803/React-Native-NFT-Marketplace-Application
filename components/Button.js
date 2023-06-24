8eed659381c9e7d58a98a302aa1705ea9980d5fb
anshika <anshikamaheshwari28@gmail.com>
2023-06-24 12:41:18 +0530

    ⚡️

--- components/Button.js
+++ components/Button.js
@@ -1,35 +1,55 @@
-import { TouchableOpacity,View, Text,Image } from 'react-native'
-import { COLORS,SIZES,FONTS,SHADOWS } from '../constants'
+import React from "react";
+import { TouchableOpacity, Text, Image } from "react-native";
 
-export const CircleButton = ({imgUrl,handlePress,...props}) => {
+import { COLORS, SIZES, FONTS, SHADOWS } from "../constants";
+
+export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
   return (
     <TouchableOpacity
-    style={{
+      style={{
         width: 40,
         height: 40,
-        backgroundColor:COLORS.white,
-        position:'absolute',
-        borderRadius:SIZES.extraLarge,
-        alignItems:'center',
-        justifyContent:'center',
+        backgroundColor: COLORS.white,
+        position: "absolute",
+        borderRadius: SIZES.extraLarge,
+        alignItems: "center",
+        justifyContent: "center",
         ...SHADOWS.light,
         ...props,
-    }}
-    onPress={handlePress}>
-        <Image
+      }}
+      onPress={handlePress}
+    >
+      <Image
         source={imgUrl}
-        resizeMode='contain'
-        style={{width:24,height:24}}/>
-
+        resizeMode="contain"
+        style={{ width: 24, height: 24 }}
+      />
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-export const RectButton = () => {
-    return (
-      <View>
-        <Text>Circle</Text>
-      </View>
-    )
-  }
-
+export const RectButton = ({ minWidth, fontSize, handlePress, ...props }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        backgroundColor: COLORS.primary,
+        padding: SIZES.small,
+        borderRadius: SIZES.extraLarge,
+        minWidth: minWidth,
+        ...props,
+      }}
+      onPress={handlePress}
+    >
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: fontSize,
+          color: COLORS.white,
+          textAlign: "center",
+        }}
+      >
+        Place a bid
+      </Text>
+    </TouchableOpacity>
+  );
+};
